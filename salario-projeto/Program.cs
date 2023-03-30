dac7516cc48dfa1d600bf568ce772d99608fb21e
Gustavo Pasqualetti <49802004863@info.local>
2023-03-30 10:15:45 -0300

    projeto salario

---
+++ salario-projeto/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write($"Informe seu salário: ");
+float salario= float.Parse(Console.ReadLine());
+
+Console.Write($"Informe seu gasto: ");
+float gasto= float.Parse(Console.ReadLine());
+
+
+if (salario > gasto)
+{
+    Console.WriteLine($"seus gastos estão dentro de seu orçamento");
+    
+}
+else
+{
+    Console.WriteLine($"seu orçamento está estourado");
+    
+}
